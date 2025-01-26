6304b3612353412bdd67c0f5d8de796698436dc0
Roopa888 <roopasree.kp@gmail.com>
2025-01-26 16:15:57 +0530

    Initial commit was blank .So added a new file and doing a second commit

--- test/tests/sample/test_sample_2.py
+++ test/tests/sample/test_sample_2.py
@@ -1,4 +1,4 @@
-# To check if the framework is properly arranged and can be run
+# To check if the framework is properly arranged and can be run #initial commit was not successful.So just added thsi new file as copy of first test_sample
 import pytest
 import allure
 import time
