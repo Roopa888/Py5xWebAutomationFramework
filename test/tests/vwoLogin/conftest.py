1bba12bd4c757f9f8488f298a814a72f15b27532
Roopa888 <roopasree.kp@gmail.com>
2025-02-11 17:29:31 +0530

    edge options updated in conftest.py

--- test/tests/vwoLogin/conftest.py
+++ test/tests/vwoLogin/conftest.py
@@ -2,15 +2,21 @@
 from selenium import webdriver
 import pytest
 from selenium.webdriver import Edge
+from selenium.webdriver.edge.options import Options as EdgeOptions
 import os
 from dotenv import load_dotenv
 load_dotenv()
-edge_options = webdriver.EdgeOptions()
-edge_options.add_argument("no-sandbox")
-edge_options.add_argument("disable-dev-shm-usage")
-edge_options.add_argument("--remote-debugging-port=9222")
-driver = webdriver.Edge(executable_path=r"msedgedriver.exe")
-driver = webdriver.Edge(options = edge_options)
+# edge_options = webdriver.EdgeOptions()
+# edge_options.add_argument("no-sandbox")
+# edge_options.add_argument("disable-dev-shm-usage")
+# edge_options.add_argument("--remote-debugging-port=9222")
+# driver = webdriver.Edge(executable_path=r"msedgedriver.exe")
+options = EdgeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--remote-debugging-port=9222")
+driver = webdriver.Edge(options =options)
+
 #driver=webdriver.Edge()
 print("driver got")
 @pytest.fixture(scope='class') #it is available to all classes
