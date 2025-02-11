3640150101d0a891a102e14a2b9ae10d737b3970
Roopa888 <roopasree.kp@gmail.com>
2025-02-11 20:15:47 +0530

    conf

--- test/tests/vwoLogin/conftest.py
+++ test/tests/vwoLogin/conftest.py
@@ -2,24 +2,19 @@
 from selenium import webdriver
 import pytest
 from selenium.webdriver import Chrome
-from selenium.webdriver.chrome.options import Options
-#from selenium.webdriver.edge.options import Options as EdgeOptions, Options
+#from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.edge.options import Options as EdgeOptions, Options
 import os
 from dotenv import load_dotenv
 load_dotenv()
-# edge_options = webdriver.EdgeOptions()
-# edge_options.add_argument("no-sandbox")
-# edge_options.add_argument("disable-dev-shm-usage")
-# edge_options.add_argument("--remote-debugging-port=9222")
-#driver = webdriver.Edge(executable_path=r"C:\Users\aslog\Downloads\edgedriver_win64 (1)\msedgedriver.exe")
-# options = EdgeOptions()
-# options.add_argument("--no-sandbox")
-# options.add_argument("--disable-dev-shm-usage")
-# options.add_argument("--remote-debugging-port=9222")
-# driver = webdriver.Edge(options =options)
-options = Options()
-options.headless = False  # Set to True if you want headless
-driver = webdriver.Chrome(options=options)
+options = EdgeOptions()
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--remote-debugging-port=9222")
+driver = webdriver.Edge(options =options)
+# options = Options()
+# options.headless = False  # Set to True if you want headless
+# driver = webdriver.Chrome(options=options)
 #driver=webdriver.Edge()
 print("driver got")
 @pytest.fixture(scope='class') #it is available to all classes
