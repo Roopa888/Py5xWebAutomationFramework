ed6c2e354f1a0a940fe5404d9822f12fd2d5320a
Roopa888 <roopasree.kp@gmail.com>
2025-03-07 17:12:48 +0530

    Webtables

--- test/tests/sample/test_sample3.py
+++ test/tests/sample/test_sample3.py
@@ -1,18 +1,15 @@
 # To check if the framework is properly arranged and can be run #initial commit was not successful.So just added thsi new file as copy of first test_sample
-import pytest
-import allure
-import time
-@allure.title("Dry run of the Framework 1")
-@allure.description("Verify that the dry run is working")
-@allure.feature("TC#0-Sample test run")
-@pytest.mark.sample
-def test_sample_pass():
-    print("Hello Sample")
-    assert True==True
-@allure.title("Dry run of the Framework 2")
-@allure.description("Verify that the dry run is working")
-@allure.feature("TC#1-Sample test run")
-@pytest.mark.sample
-def test_sample_fail():
-    print("Hello Sample")
-    assert True==True
+import chromedriver_autoinstaller
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+# Install ChromeDriver
+chromedriver_path = chromedriver_autoinstaller.install()
+
+# Initialize WebDriver
+service = Service(chromedriver_path)
+driver = webdriver.Chrome(service=service)
+# Your Selenium tests here
+driver.get("https://www.w3schools.com/")
+print(driver.title)
+driver.quit()
