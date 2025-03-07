4e1ddee9f3c1e9226f7b7e0821fb04a3476218a6
Roopa888 <roopasree.kp@gmail.com>
2025-03-07 18:42:03 +0530

    Webtables

--- test/tests/sample/test_sample3.py
+++ test/tests/sample/test_sample3.py
@@ -1,16 +1,1 @@
 # To check if the framework is properly arranged and can be run #initial commit was not successful.So just added thsi new file as copy of first test_sample
-import chromedriver_autoinstaller
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-
-# Install ChromeDriver
-chromedriver_path = chromedriver_autoinstaller.install()
-
-# Initialize WebDriver
-service = Service(chromedriver_path)
-driver = webdriver.Chrome(service=service)
-# Your Selenium tests here
-import urllib.request
-htmlfile = urllib.request.urlopen("http://google.com")
-htmltext = htmlfile.read()
-print(htmltext)
--- test/tests/vwoLogin/conftest.py
+++ test/tests/vwoLogin/conftest.py
@@ -1,12 +1,5 @@
 
 from selenium import webdriver
-import chromedriver_autoinstaller
-
-
-chromedriver_autoinstaller.install()  # Check if the current version of chromedriver exists
-                                      # and if it doesn't exist, download it automatically,
-                                      # then add chromedriver to path
-
 import pytest
 from selenium.webdriver import Chrome
 #from selenium.webdriver.chrome.options import Options
